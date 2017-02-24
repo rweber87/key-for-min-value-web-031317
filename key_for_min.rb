f8a2cc427b5373ba721b1ad447dde03a8b69faad
Robert Weber <rob.weber87@gmail.com>
2017-02-24 15:57:33 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+# 1. we need store the current value we're on
+# 2. Compare current value with all other values
+    # name_hash.length < 1 ? (return nil) : Hash[*name_hash.sort_by(&:last)[0]].each{|k, v| return k}
 
+    if name_hash.length < 1
+      return nil
+    end
+    
+      name_hash.reduce{ |f, s| f.last > s.last ? s : f }.first
 end
